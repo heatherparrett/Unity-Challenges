cad2f3712416f05833f9c90498c00f6501b94001
HeatherParrett <heather@gmail.com>
2019-12-08 23:33:55 -0500

    Upload Final Project

--- Space Shooter Tutorial/Space Shooter Tutorial/Assets/Scripts/GameController.cs
+++ Space Shooter Tutorial/Space Shooter Tutorial/Assets/Scripts/GameController.cs
@@ -7,42 +7,78 @@ using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour
 {
     public GameObject[] hazards;
+    public GameObject starfield;
+    public GameObject stardistant;
+    public float fSliderValue;
+    public float dSliderValue;
     public Vector3 spawnValues;
     public int hazardCount;
     public float spawnWait;
     public float startWait;
     public float waveWait;
+    public float speed;
 
     public Text scoreText;
     public Text restartText;
     public Text gameOverText;
     public Text winText;
+    public Text hardText;
+    public Text easyText;
     private bool gameOver;
     private bool restart;
     private int score;
+    private bool hardMode;
+    private bool easyMode;
 
+    private AudioSource win;
+    private AudioSource lose;
+    private AudioSource bgm;
+
+    private ParticleSystem starfield1;
+    private ParticleSystem starfield2;
+    
 
     void Start()
     {
         gameOver = false;
         restart = false;
+        hardMode = false;
+        easyMode = false;
         restartText.text = "";
         gameOverText.text = "";
         winText.text = "";
+        hardText.text = "Hard Mode Deactived: Press 'H'";
+        easyText.text = "Easy Mode Deactived: Press 'E'";
         score = 0;
         UpdateScore();
         StartCoroutine(SpawnWaves());
+        var source = GetComponents<AudioSource>();
+        bgm = source[0];
+        win = source[1];
+        lose = source[2];
+        bgm.Play();
+        starfield1 = GameObject.Find("part_starField").GetComponent<ParticleSystem>();
+        starfield2 = GameObject.Find("part_starField_distant").GetComponent<ParticleSystem>();
+        fSliderValue = 2.0f;
+        dSliderValue = 2.0f;
+
     }
 
     void Update()
     {
+
         if (restart)
         {
-            if (Input.GetKeyDown (KeyCode.U))
+            if (Input.GetKeyDown(KeyCode.U))
             {
                 SceneManager.LoadScene("Main");
             }
         }
+
+        var starfieldmain = starfield1.main;
+        starfieldmain.simulationSpeed = fSliderValue;
+        var starfield2main = starfield2.main;
+        starfield2main.simulationSpeed = dSliderValue;
     }
 
     void FixedUpdate()
@@ -50,6 +86,24 @@ public class GameController : MonoBehaviour
         if(Input.GetKey("escape"))
         {
             Application.Quit();
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            hardMode = true;
+            hardText.text = "Hard Mode Activated";
+            restartText.text = "Press 'U' for Restart";
+            restart = true;
+            spawnWait = 0.2f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            easyMode = true;
+            easyText.text = "Easy Mode Activated";
+            restartText.text = "Press 'U' for Restart";
+            restart = true;
+            spawnWait = 0.8f;
         }
     }
 
@@ -66,7 +120,6 @@ public class GameController : MonoBehaviour
                 Instantiate(hazard, spawnPosition, spawnRotation);
                 yield return new WaitForSeconds(spawnWait);
             }
-            yield return new WaitForSeconds(waveWait);
 
             if (gameOver)
             {
@@ -87,12 +140,27 @@ public class GameController : MonoBehaviour
     {
         scoreText.text = "Points: " + score;
 
-        if (score >= 100)
+        var scoreNeeded = 100;
+        if (hardMode == true)
+            scoreNeeded = 300;
+
+        if (easyMode == true)
+            scoreNeeded = 50;
+
+        if (score >= scoreNeeded)
         {
             winText.text = "You win! " + "Game created by Heather Parrett";
             gameOverText.text = "";
             gameOver = true;
             restart = true;
+            bgm.Stop();
+            win.Play();
+            GameObject.Find("Background").GetComponent<BGScroller>().scrollSpeed = -15;
+            fSliderValue = 20.0f;
+            dSliderValue = 20.0f;
+
+
+
         }
     }
 
@@ -100,5 +168,10 @@ public class GameController : MonoBehaviour
     {
         gameOverText.text = "Game Over!";
         gameOver = true;
+        bgm.Stop();
+        lose.Play();
+        fSliderValue = 0.0f;
+        dSliderValue = 0.0f;
+
     }
 }
